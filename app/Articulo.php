0bb2f062cf9a04336b18a71bda743a21a19daafc
Edgar Villalobos <ing.evillalobos@outlook.com>
2018-07-30 11:50:58 -0500

    ARTICULO- CREACION DE ARTICULOCONTROLLER Y RUTAS

--- app/Articulo.php
+++ app/Articulo.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Articulo extends Model
 {
-    $protected $fillable = [
+    protected $fillable = [
         'idcategoria',
         'codigo',
         'nombre',
