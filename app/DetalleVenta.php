55dd2cabbdd0e6cd8b7d2901bca2df6d0be8b4c5
Edgar Villalobos <ing.evillalobos@outlook.com>
2018-08-13 18:07:10 -0500

    VENTAS-MODELO VENTA Y DETALLEVENTA

---
+++ app/DetalleVenta.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DetalleVenta extends Model
+{
+    protected $table = 'detalle_ventas';
+    protected $fillable = [
+        'idventa',
+        'idarticulo',
+        'cantidad',
+        'precio',
+        'descuento'
+    ]
+}
